aeaaf911f5966fa94235f39160402498261bacd8
Joe Kim <jjinking@gmail.com>
2015-03-01 08:44:40 +0900

    Changed all the config settings keys containing the word 'FLASKY' to 'APP'

--- config.py
+++ config.py
@@ -12,13 +12,13 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
-    FLASKY_POSTS_PER_PAGE = 20
-    FLASKY_FOLLOWERS_PER_PAGE = 50
-    FLASKY_COMMENTS_PER_PAGE = 30
-    FLASKY_SLOW_DB_QUERY_TIME=0.5
+    APP_MAIL_SUBJECT_PREFIX = '[Flask Photo App]'
+    APP_MAIL_SENDER = 'App Admin <admin@example.com>'
+    APP_ADMIN = os.environ.get('APP_ADMIN')
+    APP_POSTS_PER_PAGE = 20
+    APP_FOLLOWERS_PER_PAGE = 50
+    APP_COMMENTS_PER_PAGE = 30
+    APP_SLOW_DB_QUERY_TIME=0.5
 
     @staticmethod
     def init_app(app):
@@ -57,9 +57,9 @@ class ProductionConfig(Config):
                 secure = ()
         mail_handler = SMTPHandler(
             mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
-            fromaddr=cls.FLASKY_MAIL_SENDER,
-            toaddrs=[cls.FLASKY_ADMIN],
-            subject=cls.FLASKY_MAIL_SUBJECT_PREFIX + ' Application Error',
+            fromaddr=cls.APP_MAIL_SENDER,
+            toaddrs=[cls.APP_ADMIN],
+            subject=cls.APP_MAIL_SUBJECT_PREFIX + ' Application Error',
             credentials=credentials,
             secure=secure)
         mail_handler.setLevel(logging.ERROR)
